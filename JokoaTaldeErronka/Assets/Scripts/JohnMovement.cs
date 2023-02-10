3e30e3180d42e06a09e8a28dc3d14c5cb02d4474
Raul Parra <parra.raul@uni.eus>
2023-02-10 12:46:25 +0100

    Controles para el PC

--- JokoaTaldeErronka/Assets/Scripts/JohnMovement.cs
+++ JokoaTaldeErronka/Assets/Scripts/JohnMovement.cs
@@ -50,8 +50,14 @@ public class JohnMovement : MonoBehaviour
         if (joystick.Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
         else if(joystick.Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
+        // para pc 
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         // si el jugador esta clickando a o d metemos la animacion
         animator.SetBool("running", joystick.Horizontal != 0.0f);
+        // para pc 
+        animator.SetBool("running", Horizontal != 0.0f);
+
 
         Debug.DrawRay(transform.position, Vector3.down*0.1f,Color.red);
         // hacemos que si esta tocando el suelo pueda saltar
@@ -109,6 +115,8 @@ public class JohnMovement : MonoBehaviour
     {
         //Vector2(2 elementos del mundo, X e Y)
         Rigidbody2D.velocity = new Vector2(joystick.Horizontal * Speed, Rigidbody2D.velocity.y);
+        //Para pc 
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
     }
     
     public void Jump()
